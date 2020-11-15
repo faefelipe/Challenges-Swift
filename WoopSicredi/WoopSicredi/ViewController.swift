a1002b7d543ef73d21205e363133ca62580aaa8c
Felipe <felipe@Felipes-MacBook-Pro.local>
2020-11-15 13:10:12 -0300

    Initial Project

---
+++ WoopSicredi/WoopSicredi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WoopSicredi
+//
+//  Created by Felipe on 15/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
