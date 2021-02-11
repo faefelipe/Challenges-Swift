b4563ac6faf32619101d2549b9c0e79ee183a94f
Felipe Almeida <fae.20@hotmail.com>
2021-02-11 15:41:37 -0300

    Add info about devices

--- MapFilterReduce/MapFilterReduce/ViewController.swift
+++ MapFilterReduce/MapFilterReduce/ViewController.swift
@@ -12,8 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let myDevices = [myImacPro, myIphone6Plus, myIphone7, myIpad, myAppleWatch, myAppleTv]
+        
+        print(principal())
     }
 
-
+    
+    var myImacPro = Device(type: "Imac Pro", price: 4999.00, color: "Space grey")
+    var myIphone6Plus = Device(type: "Iphone", price: 799.00, color: "white")
+    var myIphone7 = Device(type: "Iphone", price: 699.00, color: "Black")
+    var myIpad = Device(type: "Ipad", price: 599.00, color: "Space grey")
+    var myAppleWatch = Device(type: "Apple Watch", price: 349.00, color: "Space grey")
+    var myAppleTv = Device(type: "Apple TV", price: 199.00, color: "Black")
+    
+    
+    func principal() {
+        
+    }
 }
 
