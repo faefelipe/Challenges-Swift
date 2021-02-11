2ac7c6c5780e62c4635fb0ca501b15ac3ba8fab5
Felipe Almeida <fae.20@hotmail.com>
2021-02-11 15:28:01 -0300

    Add Struct Device

---
+++ MapFilterReduce/MapFilterReduce/Device.swift
@@ -0,0 +1,14 @@
+//
+//  Device.swift
+//  MapFilterReduce
+//
+//  Created by Paco on 11/02/21.
+//
+
+import UIKit
+
+struct Device {
+    var type: String
+    var price: Float
+    var color: String
+}
